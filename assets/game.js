511a538d472097c34f401434c6e9b532a57fae1e
jcerise <jcerise06@gmail.com>
2013-04-21 16:22:23 -0600

    Implemented actual game object, added screens file

--- assets/game.js
+++ assets/game.js
@@ -1,32 +1,24 @@
-//Check if Rot.js will work with this browser
-if (!ROT.isSupported()) {
-    alert("The Rot.js library is not supported by your browser.");
-} else {
-    //Rot.js is supported
+//Define a main game object
+var Game = {
+    _display: null,
+    init: function() {
+        //Any necessary initilization will go here
+        this._display = new ROT.Display({width:80, height:24});
+    },
+    getDisplay: function() {
+        //Return the main game display
+        return this._display;
+    }
+}
 
-    //First, we need to create a display. This will be 80 characters wide and 20 characters tall
-    var display = new ROT.Display({width:80, height:20});
-    var container = display.getContainer();
-
-    //Append the container to our HTML page
-    document.body.appendChild(container);
-
-    var foreground, background, colors
-
-    //Iterate 15 times printing some test text
-    for (var i = 0; i < 15; i ++) {
-        //Calculate the foreground color getting progressively darker,
-        //And the background color getting progressively lighter
-        foreground = ROT.Color.toRGB([255 - (i * 40),
-                                      255 - (i * 20),
-                                      255 - (i * 20)]);
-
-        background = ROT.Color.toRGB([i * 20, i * 20, i * 20]);
-
-        //Create the color format specifier
-        colors = "%c{" + foreground + "}%b{" + background + "}";
-
-        //Draw the text at column 2 and row 1
-        display.drawText(2, i, colors + "Hello world!");
+window.onload = function() {
+    //Check if Rot.js will work on this browser
+    if (!ROT.isSupported()) {
+        alert('Rot.js is not supported by this browser.');
+    } else {
+        //Initialize the game
+        Game.init();
+        //Add the display container to our HTML page
+        document.body.appendChild(Game.getDisplay().getContainer());
     }
 }
